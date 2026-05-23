00a7df75ecd23fc013eb81a7cd837432881bbc6c
ogusapjp-sketch <ogusapjp@gmail.com>
2026-05-23 19:16:14 +0900

    Fix typo in AsqiFunctionsApp to AsqiFunctionApp

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,4 +1,4 @@
 import azure.functions as func
 from main import app as fastapi_app
 
-app = func.AsqiFunctionsApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsqiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
